d61d5ff5903fe8a4a0573821b816499b469ceef9
craighunter <csh7kd@virginia.edu>
2014-04-12 00:52:59 -0400

    Multi finger recognition, multi note playback, locking to key

--- leap_midi.py
+++ leap_midi.py
@@ -2,6 +2,12 @@ from Leap import *
 import rtmidi_python as rtmidi
 from time import sleep
 
+class note:
+
+    def __init__(self, midiVal, velocity):
+        self.midiVal = midiVal
+        self.velocity = velocity
+
 def main():
     print "AWESOME MIDI CONTROLLER!"
 
@@ -11,24 +17,38 @@ def main():
     midi_out = rtmidi.MidiOut()
     midi_out.open_port(2)
 
-    last_note = None
+    playing_notes = []
+    possible_notes = [50,52,54,55,57,59,61,62]
+    possible_notes = map(lambda x: x+12,possible_notes)
+
+    for port_name in midi_out.ports:
+        print port_name
 
     while True:
         frame = leap.frame()
-        fingers = frame.fingers
-        if fingers.is_empty and last_note is not None:
-            stop_note(midi_out,last_note)
-        else:
-            f = fingers.frontmost
-            note = int(map_range(f.tip_position.y, 0, 512, 50, 70))
-            if last_note == note:
-                pass
-            else:
-                if last_note is not None:
-                    stop_note(midi_out,last_note)
-                start_note(midi_out,note)
-                last_note = note
-
+        new_notes = [] # ONLY new notes
+        continuing_notes = [] # ONLY continuing notes
+        # for each finger
+        for finger in frame.fingers:
+            # calculate note
+            note = int(map_range(finger.tip_position.x, -200, 200, 0, 7))
+            note = possible_notes[note]
+            # add to array (no duplicates)
+            if note in playing_notes and note not in continuing_notes:
+                continuing_notes.append(note)
+            elif note not in playing_notes and note not in new_notes:
+                new_notes.append(note)
+        # remove non-continuing notes
+        for old in playing_notes:
+            if old not in continuing_notes:
+                playing_notes.remove(old)
+                stop_note(midi_out,old)
+        # PLAY NEW NOTES
+        for note in new_notes:
+            start_note(midi_out,note)
+            playing_notes.append(note)
+        # PLEASE DELAY OH GOD
+        # sleep(1)
 
 # FUNCTIONS!!!!
 
